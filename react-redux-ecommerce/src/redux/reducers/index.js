b5cfee1acffa5ee5e57fccf76ef3d36c7dfd0773
Prajakta Pawar <pawar.prajakta1410@gmail.com>
2022-12-21 01:57:46 +0530

    uploading

--- react-redux-ecommerce/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { productsReducer, selectedProductsReducer } from "./productsReducer";
-const reducers = combineReducers({
-  allProducts: productsReducer,
-  product: selectedProductsReducer,
-});
-export default reducers;
